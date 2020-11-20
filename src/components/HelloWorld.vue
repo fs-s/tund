573d7c91ed193043a90d10210c596f7d6db4df6b
fs-s <joonas.puuding@ametikool.ee>
2020-11-20 11:36:34 +0200

    yes

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <select name="" id="" v-model="selected" @change="getSchedule">
+      <option
+        :value="group.groupId"
+        v-for="group in groups"
+        :key="group.groupId"
+      >
+        {{ group.groupCode }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import dayjs from "dayjs/esm/index";
+import weekOfYear from "dayjs/esm/plugin/weekOfYear";
+dayjs.extend(weekOfYear);
+export default {
+  name: "HelloWorld",
+  setup() {
+    const groups = ref([]);
+    const selected = ref(0);
+    const schedule = ref();
+    TimeTableEvents = [];
+    const week = dayjs().week() - dayjs("2020-09-01").week();
+    (async () => {
+      const res = await fetch("https://be.ta19heinsoo.itmajakas.ee/api/groups");
+      groups.value = await res.json();
+    })();
+    const getSchedule = async () => {
+      const res = await fetch(
+        `https://be.ta19heinsoo.itmajakas.ee/api/lessons/groups=${selected}&weeks=${week}`
+      );
+      schedule.value = await res.json();
+    };
+    TimeTable = computed(() => {
+      schedule.TimeTableEvents.reduce((acc, item) => {
+        if (!item.date in acc) {
+          acc[item.date] = [];
+        }
+        acc[item.date].push(item);
+        return;
+      });
+    });
+    return {
+      groups,
+      selected,
+      week,
+      getSchedule,
+      schedule,
+      TimeTable,
+    };
+  },
+};
+</script>
